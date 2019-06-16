b9e0f5eba0702c69534c80f6b1a782dfbd3fc1f4
PC-1 <abelquispeorellana1994@gmail.com>
2019-06-16 14:51:53 -0500

    prueba unitaria

---
+++ EVPUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EVPUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EVPUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea177637-1916-4fff-8e35-be7d6e09e1ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EVPUnitTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ServiceModel;
+using EVPUnitTest.AdministradorService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EVPUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                 var instChartLocal = new AdministradorService.AdministradorClient().Autenticar("12345676", "123456");
+
+                if (instChartLocal != null)
+                {
+                    Assert.AreEqual("Marlon", instChartLocal.Nombre);
+                    Assert.AreEqual("Medina", instChartLocal.Apellidos);
+                }
+
+            }
+            catch (FaultException<RepetidoException> error)
+            {
+                Assert.AreEqual("NroDocumento no existe", error.Detail.Descripcion);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            try
+            {
+                var instChartLocal = new AdministradorService.AdministradorClient().Autenticar("12345678", "00000");
+
+                if (instChartLocal != null)
+                {
+                    Assert.AreEqual("Marlon", instChartLocal.Nombre);
+                    Assert.AreEqual("Medina", instChartLocal.Apellidos);
+                }
+
+            }
+            catch (FaultException<RepetidoException> error)
+            {
+                Assert.AreEqual("Contraseña no es correcta", error.Detail.Descripcion);
+            }
+        }
+    }
+}
